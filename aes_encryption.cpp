9f805f933cdfb2b4f9d3205b743fb7babb65f7f6
Neeraj-Venugopal <neerajv03@gmail.com>
2019-05-19 02:23:30 -0700

    Constant.hpp File Addition

--- aes_encryption.cpp
+++ aes_encryption.cpp
@@ -1,10 +1,25 @@
 #include <cstdlib>
 #include <iostream>
 #include <stdlib.h>
+#include <array>
+#include <vector>
+
+#include "constant.hpp"
 
 using namespace std;
 
 int main(int argc, char** argv){
-    cout << "Hello!!" << endl;
+    cout << "Encryption with AES started" << endl;
+
+    char message[] = "This is a Test Message";
+
+    char key[16] = { 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 23, 35, 56};
+
+    
+
+    for(int i = 0; i < 10; i++){
+        cout << sBox[i] << endl;
+    }
+
     return 0;
 }
