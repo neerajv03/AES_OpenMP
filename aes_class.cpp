d6ca540512857595d2d442c006eba8c01f7574e5
Neeraj-Venugopal <neerajv03@gmail.com>
2019-05-20 07:41:06 -0400

    Encryption Completed

--- aes_class.cpp
+++ aes_class.cpp
@@ -42,40 +42,24 @@ byteArray aes_operation::aesDecrypt(byteArray encryptedData, byteArray key){
     return encryptedData;
 }
 
-byteArray aes_operation::computeSubKeys(byteArray &prev_subkey, const int &round){
-	printBigHiphen("Compute Sub Keys");
-	byteArray result(KEY_BLOCK);
-
-	result[0] = (prev_subkey[0] ^ (sBox[prev_subkey[13]] ^ RC[round]));
-	result[1] = (prev_subkey[1] ^ sBox[prev_subkey[14]]);
-	result[2] = (prev_subkey[2] ^ sBox[prev_subkey[15]]);
-	result[3] = (prev_subkey[3] ^ sBox[prev_subkey[12]]);
-
-	for (int i = 4; i != KEY_BLOCK; i += 4){
-		result[i + 0] = (result[i - 4] ^ prev_subkey[i + 0]);
-		result[i + 1] = (result[i - 3] ^ prev_subkey[i + 1]);
-		result[i + 2] = (result[i - 2] ^ prev_subkey[i + 2]);
-		result[i + 3] = (result[i - 1] ^ prev_subkey[i + 3]);
-	}
-
-	return result;
-}
 
 void aes_operation::keyAddition(byteArray &message, int round){
-	printBigHiphen("Key Addition");
+	//printBigHiphen("Key Addition");
     xorOperation(message, keys[round], KEY_BLOCK);
 }
 
+
 // Substituing for the components present in sBox Constant
 void aes_operation::subBytes(byteArray &message){
-	printBigHiphen("Sub Bytes");
+	//printBigHiphen("Sub Bytes");
 	for (int i = 0; i < KEY_BLOCK; i++){
 		message[i] = sBox[message[i]];
 	}
 }   
 
+
 void aes_operation::shiftRows(byteArray &message){
-	printBigHiphen("Shift Rows");
+	//printBigHiphen("Shift Rows");
 	byteArray tmp(KEY_BLOCK);
 
 	tmp[0] = message[0];
@@ -101,7 +85,9 @@ void aes_operation::shiftRows(byteArray &message){
 	message = tmp;
 }
 
+
 void aes_operation::mixColumns(byteArray &message){
+	//printBigHiphen("Mix Columns");
 	unsigned char b0, b1, b2, b3;
 
 	for (int i = 0; i < KEY_BLOCK; i += 4){
@@ -117,8 +103,9 @@ void aes_operation::mixColumns(byteArray &message){
 	}
 }
 
+
 void aes_operation::keySchedule(){
-	printBigHiphen("Key Schedule");
+	//printBigHiphen("Key Schedule");
 	for (int round = 0; round < 11; round++){
 		if (round == 0){
 			keys[round] = key;
@@ -126,4 +113,24 @@ void aes_operation::keySchedule(){
 		else
 			keys[round] = computeSubKeys(keys[round - 1], round - 1);	
 	}
+}
+
+
+byteArray aes_operation::computeSubKeys(byteArray &prev_subkey, const int &round){
+	//printBigHiphen("Compute Sub Keys");
+	byteArray result(KEY_BLOCK);
+
+	result[0] = (prev_subkey[0] ^ (sBox[prev_subkey[13]] ^ RC[round]));
+	result[1] = (prev_subkey[1] ^ sBox[prev_subkey[14]]);
+	result[2] = (prev_subkey[2] ^ sBox[prev_subkey[15]]);
+	result[3] = (prev_subkey[3] ^ sBox[prev_subkey[12]]);
+
+	for (int i = 4; i != KEY_BLOCK; i += 4){
+		result[i + 0] = (result[i - 4] ^ prev_subkey[i + 0]);
+		result[i + 1] = (result[i - 3] ^ prev_subkey[i + 1]);
+		result[i + 2] = (result[i - 2] ^ prev_subkey[i + 2]);
+		result[i + 3] = (result[i - 1] ^ prev_subkey[i + 3]);
+	}
+
+	return result;
 }
